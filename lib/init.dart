995b3244f2c59525f6983eeb80c01dde0b7a1ebf
newbiejh <dlwjdgns301@naver.com>
2023-03-28 15:29:09 +0900

    프로젝트 생성 및 파이어베이스 연결

---
+++ lib/init.dart
@@ -0,0 +1,26 @@
+// 로그인 정보가 이미 있는지 확인하고, 없으면 로그인 페이지, 있으면 홈페이지로 연결하는 라우터 페이지
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+class Init extends StatelessWidget {
+  const Init({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: Firebase.initializeApp(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Center(
+            child: Text("firebase load fail"),
+          );
+        }
+        if (snapshot.connectionState == ConnectionState.done) {
+          //return HomePage();
+        }
+        return CircularProgressIndicator();
+      },
+    );
+  }
+}
