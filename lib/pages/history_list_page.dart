e82ef17618d7505745f19b6fbab58d0e83afbfc7
newbiejh <dlwjdgns301@naver.com>
2023-05-06 16:29:32 +0900

    저장 내역 리스트 확인 페이지 생성

--- lib/pages/history_list_page.dart
+++ lib/pages/history_list_page.dart
@@ -1,6 +1,9 @@
 // 아바타 저장 기록 리스트 확인 페이지
 
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HistoryPage extends StatefulWidget {
   const HistoryPage({Key? key}) : super(key: key);
@@ -10,23 +13,112 @@ class HistoryPage extends StatefulWidget {
 }
 
 class _HistoryPageState extends State<HistoryPage> {
+  var data = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchHistory();
+  }
+
+  Future fetchHistory() async {
+    final response = await http
+        .get(Uri.parse('https://jsonplaceholder.typicode.com/comments'));
+
+    var list = [];
+    if (response.statusCode == 200) {
+      String responseBody = utf8.decode(response.bodyBytes);
+      list = jsonDecode(responseBody);
+    } else {
+      throw Exception('Failed to load History');
+    }
+    if (this.mounted) {
+      setState(() {
+        data = list;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon: Icon(Icons.arrow_back),
-            color: Colors.black),
-        title: Text(
-          '아바타 저장 기록 조회',
-          style: TextStyle(color: Colors.black),
+    if (data.isNotEmpty) {
+      // data 배열 비어있을 때 빨간 오류창 방지하기 위해 if문 삽입
+      return Scrollbar(
+        thickness: 4.0,
+        radius: Radius.circular(8.0),
+        child: WillPopScope(
+          onWillPop: () async => false,
+          child: Scaffold(
+            appBar: AppBar(
+              leading: IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: Icon(Icons.arrow_back),
+                  color: Colors.black),
+              title: Text(
+                '아바타 저장 기록 조회',
+                style: TextStyle(color: Colors.black),
+              ),
+              backgroundColor: Colors.yellow,
+              foregroundColor: Colors.black,
+            ),
+            body: SingleChildScrollView(
+              child: Column(
+                children: [
+                  ListView.builder(
+                    shrinkWrap: true,
+                    physics: ClampingScrollPhysics(),
+                    scrollDirection: Axis.vertical,
+                    // 스크롤 방향을 세로로 지정
+                    itemCount: data.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                          padding: EdgeInsets.symmetric(vertical: 10),
+                          child: Container(
+                              height: 40,
+                              width: double.infinity,
+                              alignment: Alignment.center,
+                              decoration: BoxDecoration(
+                                border: Border.all(color: Colors.grey),
+                                borderRadius: BorderRadius.circular(5),
+                              ),
+                              child: GestureDetector(
+                                onTap: () {
+                                  Navigator.pushNamed(context, '/check',
+                                      arguments: data[index]['id'].toString());
+                                },
+                                child: Container(
+                                  height: 40,
+                                  width: double.infinity,
+                                  decoration: BoxDecoration(
+                                    border: Border.all(color: Colors.grey),
+                                    borderRadius: BorderRadius.circular(5),
+                                  ),
+                                  child: Text('${data[index]['email']}'),
+                                ),
+                              )));
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
-        backgroundColor: Colors.yellow,
-        foregroundColor: Colors.black,
+      );
+    }
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircularProgressIndicator(),
+            SizedBox(height: 16),
+            Text('저장 기록 생성 중...'),
+          ],
+        ),
       ),
-    );
+      backgroundColor: Colors.white,
+    ); // data에 값 없으면 진행 중 마크 표시
   }
 }
