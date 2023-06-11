582c8e352bc6944f3be6f7d1fa15b98c318c9489
newbiejh <dlwjdgns301@naver.com>
2023-06-11 22:55:11 +0900

    서버 직통 get 요청 버튼 삭제

--- lib/pages/image_page.dart
+++ lib/pages/image_page.dart
@@ -98,15 +98,6 @@ class ImagePage extends StatelessWidget {
                         child: Text("아바타 이미지 업로드"),
                       ),
                       SizedBox(
-                        height: 10,
-                      ),
-                      TextButton(
-                          onPressed: () {
-                            Navigator.pushNamed(context, '/item',
-                                arguments: snapshot.data?.email);
-                          },
-                          child: Text("아이템 출력 페이지 테스트용")),
-                      SizedBox(
                         height: 30,
                       ),
                       Text(
